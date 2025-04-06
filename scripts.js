98563606080c400347b1d1007e0db8025988c5fa
Elliot Fry <iodide.05.turnips@icloud.com>
2025-04-06 15:19:53 +1000

    Initial main page build

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// const div = document.querySelector(".link");
+// const body = document.body;
+
+// div.addEventListener("mouseenter", () => {
+//     body.style.backgroundColor = "navy";
+// });
+
+// div.addEventListener("mouseleave", () => {
+//     body.style.backgroundColor = "";
+// });
